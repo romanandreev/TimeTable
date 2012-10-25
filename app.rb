5def80c8d38e9eff21ba239ebd0f3ad95903a679
lomereiter <lomereiter@gmail.com>
2012-10-25 21:52:24 +0400

    forgot to add app.rb =|

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+
+require_relative 'lib/statmod.rb'
+
+statmod = Statmod.new
+statmod.loadSpecData(File.dirname(__FILE__) + '/data/_courses/sm_spec.xml')
+statmod.loadSpecData(File.dirname(__FILE__) + '/data/_courses/mm_spec.xml')
+statmod.loadSpecData(File.dirname(__FILE__) + '/data/_courses/sa_spec.xml')
+statmod.loadStaffData(File.dirname(__FILE__) + '/data/_staff/prof.xml')
+
+get '/' do
+  @timetable = statmod.getTimeTable(File.dirname(__FILE__) + '/data/_courses/3course.xml')
+  haml :timetable
+end
