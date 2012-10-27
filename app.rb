ac0b55f948b4c8840a26c8d061ce8f336a2e6f26
lomereiter <lomereiter@gmail.com>
2012-10-27 18:09:06 +0400

    allow to see all courses

--- app.rb
+++ app.rb
@@ -9,7 +9,28 @@ statmod.loadSpecData(File.dirname(__FILE__) + '/data/_courses/mm_spec.xml')
 statmod.loadSpecData(File.dirname(__FILE__) + '/data/_courses/sa_spec.xml')
 statmod.loadStaffData(File.dirname(__FILE__) + '/data/_staff/prof.xml')
 
-get '/' do
-  @timetable = statmod.getTimeTable(File.dirname(__FILE__) + '/data/_courses/3course.xml')
+timetable3 = statmod.getTimeTable(File.dirname(__FILE__) + '/data/_courses/3course.xml')
+timetable4 = statmod.getTimeTable(File.dirname(__FILE__) + '/data/_courses/4course.xml')
+timetable5 = statmod.getTimeTable(File.dirname(__FILE__) + '/data/_courses/5course.xml')
+
+get '/3course' do
+  @timetable = timetable3
+  @course = 3
   haml :timetable
+end
+
+get '/4course' do
+  @timetable = timetable4
+  @course = 4
+  haml :timetable
+end
+
+get '/5course' do
+  @timetable = timetable5
+  @course = 5
+  haml :timetable
+end
+
+get '/' do
+  redirect '/3course'
 end
